6c7f84d843dad58766efa79bcf850e9eb215c662
CobraTalisman <iksonikenye4@gmail.com>
2024-04-16 23:48:07 -0700

    fast

--- final-project/index.php
+++ final-project/index.php
@@ -167,7 +167,7 @@ html,body,h1,h2,h3,h4 {font-family:"Lato", sans-serif}
 
     <div class="w3-third w3-margin-bottom">
       <div class="w3-card-4">
-        <img src="/w3images/team1.jpg" alt="Tommy" style="width:100%">
+        <img src="captures.webp" alt="Tommy" style="width:100%">
         <div class="w3-container">
           <h3>Tommy Egan</h3>
           <p class="w3-opacity">CEO & Founder</p>
@@ -179,7 +179,7 @@ html,body,h1,h2,h3,h4 {font-family:"Lato", sans-serif}
 
     <div class="w3-third w3-margin-bottom">
       <div class="w3-card-4">
-        <img src="/w3images/team2.jpg" alt="Hiram" style="width:100%">
+        <img src="hefee.jpg" alt="Hiram" style="width:100%">
         <div class="w3-container">
           <h3>Hiram Lodge</h3>
           <p class="w3-opacity">Art Director</p>
@@ -191,7 +191,7 @@ html,body,h1,h2,h3,h4 {font-family:"Lato", sans-serif}
 
     <div class="w3-third w3-margin-bottom">
       <div class="w3-card-4">
-        <img src="/w3images/team3.jpg" alt="Betty" style="width:100%">
+        <img src="lili.jpg" alt="Betty" style="width:100%">
         <div class="w3-container">
           <h3>Betty cooper</h3>
           <p class="w3-opacity">Designer</p>
@@ -242,7 +242,7 @@ html,body,h1,h2,h3,h4 {font-family:"Lato", sans-serif}
     <i class="fa fa-twitter w3-hover-opacity"></i>
     <i class="fa fa-linkedin w3-hover-opacity"></i>
   </div>
-  <p>Powered by <a href="web-com-s-ecommerce-solution-the best-shopify-alternative.png" title="shopify" target="_blank" class="w3-hover-text-green">Talisman.css</a></p>
+  <p>Powered by <a href="web-com-s-ecommerce-solution-the best-shopify-alternative.png" title="shopify" target="_blank" class="w3-hover-text-green">Talisman</a></p>
 </footer>
 
 
@@ -391,7 +391,7 @@ body, html {
       <p><a href="#work" class="w3-button w3-black"><i class="fa fa-th"> </i> View Our Works</a></p>
     </div>
     <div class="w3-col m6">
-      <img class="w3-image w3-round-large" src="/w3images/phone_buildings.jpg" alt="Buildings" width="700" height="394">
+      <img class="w3-image w3-round-large" src="design.jpg" alt="Website" width="700" height="394">
     </div>
   </div>
 </div>
@@ -493,7 +493,7 @@ body, html {
       <img src="tracker.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Tracker hat">
     </div>
     <div class="w3-col l3 m6">
-      <img src="sleevers.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="arm sleeve">
+      <img src="muchachos.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Happy clients">
     </div>
     <div class="w3-col l3 m6">
       <img src="steeering.webp" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Steering wheel">
@@ -525,92 +525,39 @@ body, html {
       improving the E-commerce experience.</p>
     </div>
     <div class="w3-col m6">
-      <p class="w3-wide"><i class="fa fa-camera w3-margin-right"></i>Photography</p>
+      <p class="w3-wide"><i class="fa fa-camera w3-margin-right"></i>Photoshop</p>
       <div class="w3-grey">
-        <div class="w3-container w3-dark-grey w3-center" style="width:89%">89%</div>
+        <div class="w3-container w3-dark-grey w3-center" style="width:70%">70%</div>
       </div>
       <p class="w3-wide"><i class="fa fa-desktop w3-margin-right"></i>Web Design</p>
       <div class="w3-grey">
         <div class="w3-container w3-dark-grey w3-center" style="width:80%">80%</div>
       </div>
-      <p class="w3-wide"><i class="fa fa-photo w3-margin-right"></i>Photoshop</p>
+      <p class="w3-wide"><i class="fa fa-photo w3-margin-right"></i>Photography</p>
       <div class="w3-grey">
-        <div class="w3-container w3-dark-grey w3-center" style="width:70%">70%</div>
+        <div class="w3-container w3-dark-grey w3-center" style="width:89%">89%</div>
+        <img src="pichazz.jpg" alt="photography" style="width:100%;margin-top:47px>
       </div>
     </div>
   </div>
 </div>
 
-<!-- Pricing Section -->
-<div class="w3-container w3-center w3-dark-grey" style="padding:128px 16px" id="pricing">
-  <h3>PRICING</h3>
-  <p class="w3-large">Company prices.</p>
-  <div class="w3-row-padding" style="margin-top:64px">
-    <div class="w3-third w3-section">
-      <ul class="w3-ul w3-white w3-hover-shadow">
-        <li class="w3-black w3-xlarge w3-padding-32">Basic</li>
-        <li class="w3-padding-16"><b>10GB</b> Storage</li>
-        <li class="w3-padding-16"><b>10</b> Emails</li>
-        <li class="w3-padding-16"><b>10</b> Domains</li>
-        <li class="w3-padding-16"><b>Endless</b> Support</li>
-        <li class="w3-padding-16">
-          <h2 class="w3-wide">$ 10</h2>
-          <span class="w3-opacity">per month</span>
-        </li>
-        <li class="w3-light-grey w3-padding-24">
-          <button class="w3-button w3-black w3-padding-large">Sign Up</button>
-        </li>
-      </ul>
-    </div>
-    <div class="w3-third">
-      <ul class="w3-ul w3-white w3-hover-shadow">
-        <li class="w3-red w3-xlarge w3-padding-48">Pro</li>
-        <li class="w3-padding-16"><b>25GB</b> Storage</li>
-        <li class="w3-padding-16"><b>25</b> Emails</li>
-        <li class="w3-padding-16"><b>25</b> Domains</li>
-        <li class="w3-padding-16"><b>Endless</b> Support</li>
-        <li class="w3-padding-16">
-          <h2 class="w3-wide">$ 25</h2>
-          <span class="w3-opacity">per month</span>
-        </li>
-        <li class="w3-light-grey w3-padding-24">
-          <button class="w3-button w3-black w3-padding-large">Sign Up</button>
-        </li>
-      </ul>
-    </div>
-    <div class="w3-third w3-section">
-      <ul class="w3-ul w3-white w3-hover-shadow">
-        <li class="w3-black w3-xlarge w3-padding-32">Premium</li>
-        <li class="w3-padding-16"><b>50GB</b> Storage</li>
-        <li class="w3-padding-16"><b>50</b> Emails</li>
-        <li class="w3-padding-16"><b>50</b> Domains</li>
-        <li class="w3-padding-16"><b>Endless</b> Support</li>
-        <li class="w3-padding-16">
-          <h2 class="w3-wide">$ 50</h2>
-          <span class="w3-opacity">per month</span>
-        </li>
-        <li class="w3-light-grey w3-padding-24">
-          <button class="w3-button w3-black w3-padding-large">Sign Up</button>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
+
 
 <!-- Contact Section -->
 <div class="w3-container w3-light-grey" style="padding:128px 16px" id="contact">
   <h3 class="w3-center">CONTACT</h3>
   <p class="w3-center w3-large">Lets get in touch. Send us a message:</p>
   <div style="margin-top:48px">
-    <p><i class="fa fa-map-marker fa-fw w3-xxlarge w3-margin-right"></i> Chicago, US</p>
-    <p><i class="fa fa-phone fa-fw w3-xxlarge w3-margin-right"></i> Phone: +00 151515</p>
-    <p><i class="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email: mail@mail.com</p>
+    <p><i class="fa fa-map-marker fa-fw w3-xxlarge w3-margin-right"></i> Detroit, Michigan</p>
+    <p><i class="fa fa-phone fa-fw w3-xxlarge w3-margin-right"></i> Phone: +13 4506727</p>
+    <p><i class="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email: CobraTalisman@gmail.com</p>
     <br>
     <form action="/action_page.php" target="_blank">
-      <p><input class="w3-input w3-border" type="text" placeholder="Name" required name="Name"></p>
-      <p><input class="w3-input w3-border" type="text" placeholder="Email" required name="Email"></p>
-      <p><input class="w3-input w3-border" type="text" placeholder="Subject" required name="Subject"></p>
-      <p><input class="w3-input w3-border" type="text" placeholder="Message" required name="Message"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Tommy Egan" required name="Name"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="CobraTalisman@gmail.com" required name="Email"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Fostering work ethic" required name="Subject"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Keeping up to the high standards" required name="Message"></p>
       <p>
         <button class="w3-button w3-black" type="submit">
           <i class="fa fa-paper-plane"></i> SEND MESSAGE
@@ -618,7 +565,7 @@ body, html {
       </p>
     </form>
     <!-- Image of location/map -->
-    <img src="/w3images/map.jpg" class="w3-image w3-greyscale" style="width:100%;margin-top:48px">
+    <img src="dangerzz.jpg" class="w3-image w3-greyscale" style="width:100%;margin-top:48px">
   </div>
 </div>
 
@@ -633,7 +580,7 @@ body, html {
     <i class="fa fa-twitter w3-hover-opacity"></i>
     <i class="fa fa-linkedin w3-hover-opacity"></i>
   </div>
-  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">Cobra</a></p>
 </footer>
  
 <script>
@@ -665,6 +612,7 @@ function w3_close() {
 
 </body>
 </html>
+
 
 </body>
 </html>
